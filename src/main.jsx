871175c9824d9d51bef7cf090fa1d53502a401a3
Dumbie <wasleyron668@gmail.com>
2024-09-01 15:29:55 +0500

    fixed import app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/shop-context.jsx'
-import App from './App.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
