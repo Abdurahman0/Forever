44f229b46ca1a2e8a18018ee21c894c19bde16c7
Dumbie <wasleyron668@gmail.com>
2024-09-01 15:01:20 +0500

    fixed error in exporting app component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/shop-context.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
